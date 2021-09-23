6c43336878250a417290350866ea2649e6fc360f
Soybison <coeneedell@gmail.com>
2021-09-23 18:58:54 -0400

    heroku basics

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_socketio import SocketIO
+import os
+
 
 app = Flask(__name__)
-
+app.config['DATABASE_URL'] = os.environ['DATABASE_URL']
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return "Hello World!!"
+    return "Lmao you shouldn't be here."
+
+
+if __name__ == '__main__':
+    socketio.run(app)
